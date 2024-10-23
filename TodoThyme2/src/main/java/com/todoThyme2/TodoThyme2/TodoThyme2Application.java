41f93ea572f84ff3c031c1c03136146b3411cce9
Niklas <Niklasgth@hotmail.com>
2024-10-23 14:02:48 +0200

    Add files via upload

---
+++ TodoThyme2/src/main/java/com/todoThyme2/TodoThyme2/TodoThyme2Application.java
@@ -0,0 +1,13 @@
+package com.todoThyme2.TodoThyme2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoThyme2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoThyme2Application.class, args);
+	}
+
+}
---
+++ TodoThyme2/src/test/java/com/todoThyme2/TodoThyme2/TodoThyme2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoThyme2.TodoThyme2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoThyme2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
